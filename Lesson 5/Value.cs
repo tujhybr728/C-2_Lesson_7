9501c1b844b3bdbbdd9db9e0e51b401030e7c9cc
Rogier <tujhybr728@mail.ru>
2019-03-15 01:22:01 +0300

    Функционально, ничего не изменилось, но теперь есть база и независимое приложение которое её заполняет.

---
+++ Lesson 5/Value.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_5
+{
+    class Value
+    {
+        //public MainWindow Main = new MainWindow();
+        //public string[,] Listall;
+        public string path = "List.txt";
+        public int Chek;
+        public string line;
+        public List<Escl> Listall = new List<Escl>();
+
+        public void Search()
+        {
+
+        }
+
+        public void NewEsc()
+        {
+
+        }
+
+        public void Retrac()
+        {
+                        
+        }
+
+        public void SaveAll() // сохранение списка в файл
+        {
+            StreamWriter sw1 = new StreamWriter(path, false, System.Text.Encoding.Default);
+            sw1.Write("");
+            StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default);
+
+            for (int i = 0; i < Listall.LongCount(); i++) // используется список
+            {
+                sw.WriteLine(Listall.GetType());
+                Listall.RemoveAt(i);
+            }
+
+
+        }
+
+        public void RewewAll() // выгрузка списка из файла
+        {
+            
+            Chek = 0;
+            StreamReader sr = new StreamReader(path);
+            while ((line = sr.ReadLine()) != null)
+                Chek++;
+            //Listall = new string[Chek/2, 2];
+            Chek = 0;
+            while(!sr.EndOfStream)
+            //for (int i = 0; i<Listall.LongLength; i++)
+            // {
+            //     line = sr.ReadLine();
+            //     if ((i % 2) == 0) Listall[i, 1] = line;
+            //     else Listall[i-1, 2] = line;
+            // }
+            {
+                Listall.Add(new Escl(sr.ReadLine(), sr.ReadLine()));
+            }
+            sr.Close();
+        }
+        
+    }
+}
